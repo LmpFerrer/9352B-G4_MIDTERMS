6a38a81dc57169e4362b55920669bf9ed5528ab4
Lmpf <2165944@slu.edu.ph>
2018-05-02 06:04:18 +0800

    updates on the design

--- www/script.js
+++ www/script.js
@@ -12,12 +12,12 @@ var arr;
 var requestUrl = 'js/datass.json';
 var request = new XMLHttpRequest();
 
-window.onload = function() {
-	initWidth = document.documentElement.clientWidth;
-	request.open('GET', requestUrl);
-	request.responseType = 'json';
-	request.send();
-	loadMap();
+window.onload = function () {
+    initWidth = document.documentElement.clientWidth;
+    request.open('GET', requestUrl);
+    request.responseType = 'json';
+    request.send();
+    loadMap();
 };
 
 function openNav() {
@@ -30,170 +30,171 @@ function closeNav() {
 
 //load map
 function loadMap() {
-	var base_image = new Image();
-	base_image.src = 'map/zoom15.bmp';
-	base_image.onload = function() {
-		imgwid = base_image.width;
-		imghgt = base_image.height;
-		context.canvas.width = imgwid;
-		context.canvas.height = imghgt;
-	};
-	
-	request.onload = function() {
-		console.log("Storing elements in array");
-		house = request.response;
-		console.log(house);
-		house.forEach(function(datass){
-			houses.push({
-			Name: datass.Name,
-			Capacity:datass.Capacity,
-			Price:datass.Price,
-			Address:datass.Address,
-			y:parseInt(datass.Latitude, 10)+50,
-			x:parseInt(datass.Longtitude, 10)+400,
-			height:30,
-            width:30
-			});
-		});
-	}
+    var base_image = new Image();
+    base_image.src = 'map/zoom15.bmp';
+    base_image.onload = function () {
+        imgwid = base_image.width;
+        imghgt = base_image.height;
+        context.canvas.width = imgwid;
+        context.canvas.height = imghgt;
+    };
+
+    request.onload = function () {
+        console.log("Storing elements in array");
+        house = request.response;
+        console.log(house);
+        house.forEach(function (datass) {
+            houses.push({
+                Name: datass.Name,
+                Capacity: datass.Capacity,
+                Price: datass.Price,
+                Address: datass.Address,
+                y: parseInt(datass.Latitude, 10) + 50,
+                x: parseInt(datass.Longtitude, 10) + 400,
+                height: 30,
+                width: 30
+            });
+        });
+    }
 };
 
 //adding markers to the map
-function addMarkerPoints(x,y){
-	var marker = new Image();
-	marker.src = './images/marker.png';
-	var canvas = document.getElementById('map'),
-	context = canvas.getContext('2d');
-	context.drawImage(marker,x,y,30,30);
-    
+function addMarkerPoints(x, y) {
+    var marker = new Image();
+    marker.src = './images/marker.png';
+    var canvas = document.getElementById('map'),
+        context = canvas.getContext('2d');
+    context.drawImage(marker, x, y, 30, 30);
+
 };
 
 //setting the points according to the array
 function locations(collections) {
-	collections.forEach(function(datass) {
-		addMarkerPoints(datass.x,datass.y);
-	});
+    collections.forEach(function (datass) {
+        addMarkerPoints(datass.x, datass.y);
+    });
 }
 
 //click area
-var clickHouse = function(event) {
-	var x = event.pageX - elemLeft;
-	var y = event.pageY - elemTop;
-	houses.forEach(function(datass) {
-		if (y > element.y && y < element.y + element.height 
-            && x > element.x && x < element.x + element.width) {
-			var balay = datass.Name;
-			buildContent(balay);
-		}
-	});
+var clickHouse = function (event) {
+    var x = event.pageX - elemLeft;
+    var y = event.pageY - elemTop;
+    houses.forEach(function (datass) {
+        if (y > element.y && y < element.y + element.height &&
+            x > element.x && x < element.x + element.width) {
+            var balay = datass.Name;
+            buildContent(balay);
+        }
+    });
 }
 
-var addClickEvent = function() {
-	area.addEventListener('click', clickHouse, false);
+var addClickEvent = function () {
+    area.addEventListener('click', clickHouse, false);
 }
 
-var removeClickEvent = function() {
-	area.removeEventListener('click', clickHouse, false);
+var removeClickEvent = function () {
+    area.removeEventListener('click', clickHouse, false);
 }
 //shows the click content
 function buildContent(name) {
-	var container = document.getElementById('rest-container');
-	container.className = "show";
-	houses.forEach(function(datass) {
-		if(name == datass.Name) {
-			container.innerHTML = createMenu(datass.Name, datass.Capacity, datass.Price, datass.Address);
-		}
-	});
+    var container = document.getElementById('rest-container');
+    container.className = "show";
+    houses.forEach(function (datass) {
+        if (name == datass.Name) {
+            container.innerHTML = createMenu(datass.Name, datass.Capacity, datass.Price, datass.Address);
+        }
+    });
 }
-//returns a innerHTML
+//returns an innerHTML
 function createMenu(Name, Capacity, Price, Address) {
-	var html = `<div class="menu">
+    var html = `<div class="menu">
+    <span class="close">&times;</span><br>
 	<h1>${Name}</h1>
-	<h3>Capacity: ${Capacity}</h3>
-	<h3>Price: ${Price}</h3>
-	<h3>Address: </br> ${Address}</h3>
+	<h4><i>Capacity:</i> ${Capacity}</h4>
+	<h4><i>Price:</i> ${Price}</h4>
+	<h4><i>Address:</i> </br> ${Address}</h4>
 	</div>`
-	return html;
+    return html;
 }
 
-var otherClickEvent = function(collections) {
-	area.addEventListener('click', otherHouses(collections), false);
+var otherClickEvent = function (collections) {
+    area.addEventListener('click', otherHouses(collections), false);
 }
 
-var removeOtherClickEvent = function(collections) {
-	area.removeEventListener('click', otherHouses(collections), false);
+var removeOtherClickEvent = function (collections) {
+    area.removeEventListener('click', otherHouses(collections), false);
 }
 
 //some houses only
 function otherHouses(collections) {
-	return function(event) {
-		var x = event.pageX - elemLeft;
-		var y = event.pageY - elemTop;
-		collections.forEach(function(datass) {
-			if (y > datass.y && y < datass.y + datass.height 
-				&& x > datass.x && x < datass.x + datass.width) {
-				var balay = datass.Name;
-				buildContent(balay);
-			}
-		});
-	}
+    return function (event) {
+        var x = event.pageX - elemLeft;
+        var y = event.pageY - elemTop;
+        collections.forEach(function (datass) {
+            if (y > datass.y && y < datass.y + datass.height &&
+                x > datass.x && x < datass.x + datass.width) {
+                var balay = datass.Name;
+                buildContent(balay);
+            }
+        });
+    }
 }
 
 //show per category
 var prices = document.getElementsByClassName('price');
-for(var i = 0; i < prices.length; i++) {
-	prices[i].addEventListener('click', function() {
-		removeClickEvent();
-		removeOtherClickEvent(arr);
-		var tempValue = this.value;
-		context.clearRect(0, 0, imgwid, imghgt);
-		arr = [];
-		if(tempValue == low){
-			house.forEach(function(datass) {
-				if(parseInt(datass.Price, 10)>=3000) {
-					arr.push({
-						Name: datass.Name,
-						x:parseInt(datass.Latitude, 10)+400,
-						y:parseInt(datass.Longtitude, 10)+50,
-						radius: 15
-					});
-				}
-			});
-		}
-		if(tempValue == medium){
-			house.forEach(function(datass) {
-				if(parseInt(datass.Price, 10)<3000 && parseInt(datass.Price, 10)>=5000) {
-					arr.push({
-						Name: datass.Name,
-						x:parseInt(datass.Latitude, 10)+400,
-						y:parseInt(datass.Longtitude, 10)+50,
-						radius: 15
-					});
-				}
-			});
-		}
-		if(tempValue == high){
-			house.forEach(function(datass) {
-				if(parseInt(datass.Price, 10)<5000) {
-					arr.push({
-						Name: datass.Name,
-						x:parseInt(datass.Latitude, 10)+400,
-						y:parseInt(datass.Longtitude, 10)+50,
-						radius: 15
-					});
-				}
-			});
-		}
-		locations(arr);
-		otherClickEvent(arr);
-	});
+for (var i = 0; i < prices.length; i++) {
+    prices[i].addEventListener('click', function () {
+        removeClickEvent();
+        removeOtherClickEvent(arr);
+        var tempValue = this.value;
+        context.clearRect(0, 0, imgwid, imghgt);
+        arr = [];
+        if (tempValue == low) {
+            house.forEach(function (datass) {
+                if (parseInt(datass.Price, 10) >= 3000) {
+                    arr.push({
+                        Name: datass.Name,
+                        x: parseInt(datass.Latitude, 10) + 400,
+                        y: parseInt(datass.Longtitude, 10) + 50,
+                        radius: 15
+                    });
+                }
+            });
+        }
+        if (tempValue == medium) {
+            house.forEach(function (datass) {
+                if (parseInt(datass.Price, 10) < 3000 && parseInt(datass.Price, 10) >= 5000) {
+                    arr.push({
+                        Name: datass.Name,
+                        x: parseInt(datass.Latitude, 10) + 400,
+                        y: parseInt(datass.Longtitude, 10) + 50,
+                        radius: 15
+                    });
+                }
+            });
+        }
+        if (tempValue == high) {
+            house.forEach(function (datass) {
+                if (parseInt(datass.Price, 10) < 5000) {
+                    arr.push({
+                        Name: datass.Name,
+                        x: parseInt(datass.Latitude, 10) + 400,
+                        y: parseInt(datass.Longtitude, 10) + 50,
+                        radius: 15
+                    });
+                }
+            });
+        }
+        locations(arr);
+        otherClickEvent(arr);
+    });
 }
 
 //Click Transients
-document.getElementById("Transients").addEventListener("click", function (){
-	locations(houses);
+document.getElementById("Transients").addEventListener("click", function () {
+    locations(houses);
     otherClickEvent(houses);
-}); 
+});
 
 //to clear the innerhtml
 function clear() {
@@ -201,37 +202,37 @@ function clear() {
 }
 
 //Click Baguio
-document.getElementById("Baguio").addEventListener("click", function (){
-	context.clearRect(0, 0, imgwid, imghgt);
-	removeOtherClickEvent();
-	clear();
+document.getElementById("Baguio").addEventListener("click", function () {
+    context.clearRect(0, 0, imgwid, imghgt);
+    removeOtherClickEvent();
+    clear();
 });
 //collecting information
-var clickMeh = function getData(){
-	var data = document.getElementById("form");
-	var name = data.elements[0].value;
-	var capacity = data.elements[1].value;
-	var price = data.elements[2].value;
+var clickMeh = function getData() {
+    var data = document.getElementById("form");
+    var name = data.elements[0].value;
+    var capacity = data.elements[1].value;
+    var price = data.elements[2].value;
 }
 
 //Click Search
-document.getElementById("Search").addEventListener("click", function (){
-	var x = document.getElementById("Search").value;
-	removeClickEvent();
-	removeOtherClickEvent(arr);
-	var tempValue = this.value;
-	context.clearRect(0, 0, imgwid, imghgt);
-	arr = [];
-	if(x == datass.Name) {
-		arr.push({
-		Name: datass.Name,
-		x:parseInt(datass.Latitude, 10)+400,
-		y:parseInt(datass.Longtitude, 10)+50,
-		radius: 15
-		});
-	}
-	locations(arr);
-	otherClickEvent(arr);
+document.getElementById("Search").addEventListener("click", function () {
+    var x = document.getElementById("Search").value;
+    removeClickEvent();
+    removeOtherClickEvent(arr);
+    var tempValue = this.value;
+    context.clearRect(0, 0, imgwid, imghgt);
+    arr = [];
+    if (x == datass.Name) {
+        arr.push({
+            Name: datass.Name,
+            x: parseInt(datass.Latitude, 10) + 400,
+            y: parseInt(datass.Longtitude, 10) + 50,
+            radius: 15
+        });
+    }
+    locations(arr);
+    otherClickEvent(arr);
 });
 
 // Get the modal
@@ -243,22 +244,16 @@ var btn = document.getElementById("Search");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+var x = document.getElementById('rest-container');
+
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
-};
 
-//Call Service Worker
-if('serviceWorker' in navigator){
-    try {
-        navigator.serviceWorker.register('sw.js');
-        console.log('SW resgitered');
-    } catch (error) {
-        console.log('SW registration failed');
-    }
+    x.className = "hide";
 };
